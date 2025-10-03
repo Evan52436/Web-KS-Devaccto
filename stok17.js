fb112d2babca09adc8a91c7466001460b2182ef3
unboiledpotato <evan.pranawaa@gmail.com>
2025-10-03 22:06:34 +0700

    Add files via upload

---
+++ stok17.js
@@ -0,0 +1,16 @@
+function minus () {
+    document.getElementById('jumlah').value --;
+    if (document.getElementById('jumlah').value > 0)
+        return;
+    else {
+        document.getElementById('jumlah').value = 1;
+        alert("lu beli apa jir quantity minus");
+    }
+}
+
+function tambah () {
+        document.getElementById('jumlah').value ++;
+    if (document.getElementById('jumlah').value > 16)
+        alert("maaf stok nya terbatas, hanya tersedia 17 item" );
+    
+}
---
+++ stok2.js
@@ -0,0 +1,15 @@
+function minus () {
+    document.getElementById('jumlah').value --;
+    if (document.getElementById('jumlah').value > 0)
+        return;
+    else {
+        document.getElementById('jumlah').value = 1;
+        alert("lu beli apa jir quantity minus");
+    }
+}
+
+function tambah () {
+        document.getElementById('jumlah').value ++;
+        if (document.getElementById('jumlah').value > 2);
+        alert("maaf stok nya terbatas, hanya tersedia 2 item" );
+}
